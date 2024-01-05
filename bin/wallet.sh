7cece2762249bda68c30bd2ca11a7ebd91ab1568
lunacrafts <lunacrafts@protonmail.com>
2024-01-05 14:07:06 +0100

    wallet.sh

---
+++ bin/wallet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Service name in the docker-compose file
+SERVICE_NAME="r_squared_core"
+
+# Check if the docker-compose service is running
+if docker-compose ps | grep -q $SERVICE_NAME; then
+    echo "Attaching to the $SERVICE_NAME service..."
+    docker-compose exec $SERVICE_NAME /bin/sh -c "cd /var/lib/rsquared && /usr/local/bin/cli_wallet"
+else
+    echo "Service $SERVICE_NAME is not running."
+fi
