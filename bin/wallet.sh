eb1d3b294a68a77c8be0cec9568067f9616ef973
lunacrafts <lunacrafts@protonmail.com>
2024-01-22 11:51:06 +0100

    wallet

--- bin/wallet.sh
+++ bin/wallet.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Service name in the docker-compose file
-SERVICE_NAME="r_squared_core"
+SERVICE_NAME="axxxxxxm_wallet"
 
 # Check if the docker-compose service is running
 if docker-compose ps | grep -q $SERVICE_NAME; then
     echo "Attaching to the $SERVICE_NAME service..."
-    docker-compose exec $SERVICE_NAME /bin/sh -c "cd /var/lib/rsquared && /usr/local/bin/cli_wallet"
+    docker-compose exec $SERVICE_NAME /bin/sh -c "mkdir -p /var/lib/wallet && cd /var/lib/wallet && /usr/local/bin/cli_wallet"
 else
     echo "Service $SERVICE_NAME is not running."
 fi
