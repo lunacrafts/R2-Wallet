70a137e3313a9cde84baaf06aedf056114cd8a75
lunacrafts <lunacrafts@protonmail.com>
2024-02-03 17:12:39 +0100

    Attach / wallet bin/sh bin/bash

--- bin/attach.sh
+++ bin/attach.sh
@@ -6,7 +6,9 @@ SERVICE_NAME="r2_wallet"
 # Check if the docker-compose service is running
 if docker-compose ps | grep -q $SERVICE_NAME; then
     echo "Attaching to the $SERVICE_NAME service..."
-    docker-compose exec $SERVICE_NAME /bin/sh -c "cd /var/lib/rsquared && /bin/sh"
+    if ! docker-compose exec $SERVICE_NAME /bin/sh -c "cd /var/lib/rsquared && /bin/sh"; then
+        docker-compose exec $SERVICE_NAME /bin/bash -c "cd /var/lib/rsquared && /bin/bash"
+    fi
 else
     echo "Service $SERVICE_NAME is not running."
 fi
--- bin/wallet.sh
+++ bin/wallet.sh
@@ -3,10 +3,22 @@
 # Service name in the docker-compose file
 SERVICE_NAME="r2_wallet"
 
+# Function to execute a command in the service with a specified shell
+execute_in_service_with_shell() {
+    local shell=$1
+    echo "Attempting to use $shell..."
+    docker-compose exec $SERVICE_NAME $shell -c "mkdir -p /var/lib/wallet && cd /var/lib/wallet && /usr/local/bin/cli_wallet"
+}
+
 # Check if the docker-compose service is running
 if docker-compose ps | grep -q $SERVICE_NAME; then
     echo "Attaching to the $SERVICE_NAME service..."
-    docker-compose exec $SERVICE_NAME /bin/sh -c "mkdir -p /var/lib/wallet && cd /var/lib/wallet && /usr/local/bin/cli_wallet"
+    # First try with /bin/sh
+    if ! execute_in_service_with_shell "/bin/sh"; then
+        echo "/bin/sh failed, trying /bin/bash..."
+        # If /bin/sh fails, then try with /bin/bash
+        execute_in_service_with_shell "/bin/bash"
+    fi
 else
     echo "Service $SERVICE_NAME is not running."
 fi
